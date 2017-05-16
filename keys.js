fdd0eaabb0ff088b20edbfb59953b0e166c9de68
drewmichinock <drewmichinock@gmail.com>
2017-05-16 16:30:36 -0700

    1st commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '0L5IyDiUwu8Wldxuj4C1T64cK',
+  consumer_secret: '4RxWboKbTSGZr9jSYlkB0YaHKsLZC6oD2AuH2FryQRFS42OBcx',
+  access_token_key: '864620914645753857-J2MrZxab9eLG4VoLCi1zkGuoFvlVIfX',
+  access_token_secret: 'wsigEqdtsh8lfYw2gMLjisFHGU4G6dgcNf6pM0qi1ydGU',
+}
