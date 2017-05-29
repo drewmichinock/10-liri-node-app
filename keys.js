e7c679aa327a35ef80906900d9668264280a783d
drewmichinock <drewmichinock@gmail.com>
2017-05-28 22:40:06 -0700

    2nd commit

--- keys.js
+++ keys.js
@@ -1,8 +1,16 @@
 console.log('this is loaded');
 
+// twitter api keys
 exports.twitterKeys = {
   consumer_key: '0L5IyDiUwu8Wldxuj4C1T64cK',
   consumer_secret: '4RxWboKbTSGZr9jSYlkB0YaHKsLZC6oD2AuH2FryQRFS42OBcx',
   access_token_key: '864620914645753857-J2MrZxab9eLG4VoLCi1zkGuoFvlVIfX',
   access_token_secret: 'wsigEqdtsh8lfYw2gMLjisFHGU4G6dgcNf6pM0qi1ydGU',
 }
+
+// omdb api keys
+// 37326a07
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=37326a07
+// Poster API: http://img.omdbapi.com/?i=tt3896198&h=600&apikey=37326a07
+
+// spotify api keys
--- liri.js
+++ liri.js
@@ -1,5 +1,92 @@
-// At the top of the liri.js file, 
 // write the code you need to grab the data from keys.js. 
 
+var Twitter = require("twitter");
 
-// Then store the keys in a variable.
+var keys = require("./keys.js");
+
+var client = new Twitter(
+
+    keys.twitterKeys
+
+);
+
+var request = require("request");
+
+var args = process.argv.slice(2);
+
+if (args[0] === "my-tweets") {
+
+    doTweets();
+
+} else if (args[0] === "spotify-this-song") {
+
+    doSpotify();
+
+} else if (args[0] === "movie-this") {
+
+    doOMDB();
+
+} else if (args[0] === "do-what-it-says") {
+
+    doText();
+
+} else {
+
+    console.log("missing argument");
+
+    return;
+
+}
+
+
+function doTweets() {
+
+    var params = {screen_name: 'drew_michinock'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+
+        var tweetText = tweets.map(function(tweet) {
+
+            return tweet.text;
+
+        });
+
+        console.log(tweetText);
+    }
+    });
+    
+}
+ 
+function doSpotify() {
+
+    
+}
+
+function doOMDB() {
+
+    var title = args[1] || "Mr. Nobody";
+
+    request("http://www.omdbapi.com/?apikey=37326a07&t=" + title, function(error, response, body){
+
+        if (!error) {
+
+            var body = JSON.parse(body);
+
+            console.log("Title: " + body.Title);
+            console.log("Year: " + body.Year);
+            console.log("IMDb Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("IMDb URL: " + "http://www.imdb.com/title/" + body.imdbID + "/");
+
+        }
+
+    });
+    
+}
+
+function doText() {
+
+}
